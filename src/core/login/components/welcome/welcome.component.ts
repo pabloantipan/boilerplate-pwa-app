9eb9856e4f1b0f057d07846996149060b8000239
Pablo Antipan <antipan.pablo@gmail.com>
2025-01-31 19:17:19 -0300

    [ADD] ionic base welcome component

--- src/core/login/components/welcome/welcome.component.ts
+++ src/core/login/components/welcome/welcome.component.ts
@@ -1,9 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, forwardRef } from '@angular/core';
+import {
+  NG_VALUE_ACCESSOR,
+  ReactiveFormsModule
+} from '@angular/forms';
 import { Router } from '@angular/router';
+import {
+  IonButton, IonCard, IonCardContent,
+  IonCardHeader, IonCardSubtitle,
+  IonCardTitle, IonContent,
+  IonInput, IonItem, IonLabel
+} from "@ionic/angular/standalone";
 import { SessionProvider } from '@login/providers/session.provider';
 
 @Component({
   selector: 'app-welcome',
+  standalone: true,
+  imports: [
+    CommonModule,
+    IonButton,
+    IonCardContent,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonCardHeader,
+    IonCard,
+    IonLabel,
+    IonItem,
+    IonContent,
+    IonInput,
+    ReactiveFormsModule,
+  ],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => IonInput),
+      multi: true,
+    },
+  ],
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss'],
 })
